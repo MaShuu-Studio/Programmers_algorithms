d30f39b24a08ee40f819844a30b9566f13e247f6
Shuu <ghwls328@naver.com>
2021-01-14 14:21:26 +0900

    문자열을 정수로 바꾸기 (stoi 부호처리 필요X)

--- level 1/12925.cpp
+++ level 1/12925.cpp
@@ -4,14 +4,5 @@
 using namespace std;
 
 int solution(string s) {
-    int a = 1;
-    
-    if (s[0] == '+') s = s.substr(1, s.length());
-    else if (s[0] == '-')
-    {
-        s = s.substr(1, s.length());
-        a = -1;
-    }
-    
-    return stoi(s) * a;
+    return stoi(s);
 }
