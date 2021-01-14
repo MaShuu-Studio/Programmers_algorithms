be1dabff179dd16893615aa6f4460fdaffad8066
Shuu <ghwls328@naver.com>
2021-01-14 18:35:56 +0900

    자릿수 더하기

---
+++ level 1/12931.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    string str = to_string(n);
+    
+    for (int i = 0; i < str.length(); i++)
+        answer += (str[i] - '0');    
+
+    return answer;
+}
