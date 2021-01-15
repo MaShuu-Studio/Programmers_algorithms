02f662b59811c9849dc3887fd331065be157b448
Shuu <ghwls328@naver.com>
2021-01-15 22:23:18 +0900

    124 나라의 숫자

--- level 2/12899.cpp
+++ level 2/12899.cpp
@@ -8,22 +8,15 @@ string solution(int n) {
     string answer = "";
     while(n > 0)
     {
-        answer += to_string(n%3);
+        int r = n % 3;
         n /= 3;
-    }
-    
-    for (int i = 0; i < answer.length()-1; i++)
-    {
-        if (answer[i] == '0' && answer[i+1] != '0')
+
+        if (r == 0)
         {
-            answer[i] = '4';
-            answer[i+1] -= 1;
+            n--;
+            answer = "4" + answer;
         }
+        else answer = to_string(r) + answer;
     }
-    
-    if (answer[answer.length()-1] == '0')
-        answer.pop_back();
-    
-    reverse(answer.begin(), answer.end());
     return answer;
 }
